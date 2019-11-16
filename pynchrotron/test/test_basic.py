7ce49d0e9bdb26f3bb27778c9e419740ef28c91c
grburgess <jmichaelburgess@gmail.com>
2019-11-16 08:54:14 +0100

    added test for cheb

--- pynchrotron/test/test_basic.py
+++ pynchrotron/test/test_basic.py
@@ -1,4 +1,21 @@
 from pynchrotron import synchrotron_kernel
+from pynchrotron.synchrotron_kernel import cheb_eval
+
+
+import numpy as np
+
+
+def test_cheb():
+
+
+    data = np.array([1E-4,1E-2,1.])
+
+    val = cheb_eval(data,2,-1,1,.1)
+
+    np.testing.assert_almost_equal(val, -0.97895)
+    
+
+
 
 def test_synchrotron_kernel():
 
