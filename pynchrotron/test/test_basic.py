f1454aa183c098145619194a3acaf3067135a9b0
grburgess <jmichaelburgess@gmail.com>
2019-11-16 09:10:55 +0100

    trying to resolve versions

--- pynchrotron/test/test_basic.py
+++ pynchrotron/test/test_basic.py
@@ -1,6 +1,6 @@
 from pynchrotron import synchrotron_kernel
 from pynchrotron.synchrotron_kernel import cheb_eval
-from hypothesis import given
+from hypothesis import given, settings
 import hypothesis.strategies as st
 
 import numpy as np
@@ -16,8 +16,9 @@ def test_cheb():
     np.testing.assert_almost_equal(val, -0.97895)
     
 
-    
 
+    
+@settings(deadline=None)
 @given(st.floats(min_value=1E-100) )
 def test_synchrotron_kernel(val):
 
