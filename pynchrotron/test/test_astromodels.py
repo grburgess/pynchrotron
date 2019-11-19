1211db671a1bec5ac7c6aa130503fe1f13c53620
grburgess <jmichaelburgess@gmail.com>
2019-11-19 12:27:42 +0100

    added test with units

--- pynchrotron/test/test_astromodels.py
+++ pynchrotron/test/test_astromodels.py
@@ -1,10 +1,25 @@
 from pynchrotron import SynchrotronNumerical
 import numpy as np
 
-def test_astromodels_model():
+from astromodels import PointSource, Model
+
+import astropy.units as u
+
+
+def test_astromodels_model_without_units():
 
     x = SynchrotronNumerical()
 
     ene = np.logspace(1,5, 100)
 
     x(ene)
+
+def test_astromodels_model_with_units():
+
+    x = SynchrotronNumerical()
+
+    ene = np.logspace(1,5, 5) * u.keV
+
+    model = Model(PointSource('test',0,0,spectral_shape=x))
+
+    model.get_point_source_fluxes(0,ene)
