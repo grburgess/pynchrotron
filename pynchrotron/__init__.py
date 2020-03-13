b0685dbdae0bff6711c6dc0f9974bd9614719f89
grburgess <jmichaelburgess@gmail.com>
2020-03-13 16:31:26 +0100

    update version

--- pynchrotron/__init__.py
+++ pynchrotron/__init__.py
@@ -3,3 +3,7 @@ from .synchrotron_kernel import synchrotron_kernel
 from .threeML_models import SynchrotronNumerical
 
 __all__ = ['cool_and_radiate', 'synchrotron_kernel', 'SynchrotronNumerical']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
